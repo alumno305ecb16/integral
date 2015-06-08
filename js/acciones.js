9c98724279704e76e9e80da667a07f0bc9050a72
alumno305ecb16 <scorpions130295@gmail.com>
2015-06-08 16:01:44 -0500

    8

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 $(document).ready(function(e) {	
-//document.addEventListener("deviceready",function(){
+document.addEventListener("deviceready",function(){
 	
 		 
 		 
@@ -436,4 +436,4 @@ $('.n').on('click', function (){
 
 
 });
-//});
+});
